b7493b31268e98cb891586b10dd2f1ebf8a7479a
himanshubector <smk.131@gmail.com>
2023-09-19 02:24:00 +0200

    first commit

---
+++ src/main/java/com/himanshu/aws/cicd/example/OrderDao.java
@@ -0,0 +1,23 @@
+package com.himanshu.aws.cicd.example;
+
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Repository
+public class OrderDao
+{
+    public List<Order> getOrders()
+    {
+        return Stream.of(
+                        new Order(101, "Mobile", 1, 35000),
+                        new Order(58, "Book", 4, 2000),
+                        new Order(205, "Laptop", 1, 170000),
+                        new Order(809, "headset", 1, 1499))
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ src/test/java/com/himanshu/aws/cicd/example/AwsdevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshu.aws.cicd.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsdevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
